ee531e1a494408a08a5a7d0237e9ff31cfe0bf12
alleballer <albert.banke@gmail.com>
2023-05-29 20:45:31 +0200

    saving

--- app.py
+++ app.py
@@ -15,24 +15,20 @@ for filename in filenames:
 
 # Concatenate all the dataframes
 df = pd.concat(df_list)
+
 def main():
     st.title('My Modeling Results')
 
-    # Use a select box for user to select a column to view statistics
-    st.sidebar.title('Select a column to view statistics')
-    select_box = st.sidebar.selectbox('Columns', df.columns.tolist(), index=0)
-    st.sidebar.write(df[select_box].describe())
+    # Use a select box for user to select a target to view model results
+    st.sidebar.title('Select a target to view model results')
+    select_box = st.sidebar.selectbox('Targets', df['target'].unique().tolist(), index=0)
+    selected_target_df = df[df['target'] == select_box]
 
-    # Use a select box for user to select a data type
-    st.sidebar.title('Select a data type')
-    data_select_box = st.sidebar.selectbox('Data', df['data'].unique().tolist(), index=0)
-    selected_data_df = df[df['data'] == data_select_box]
+    # Display model results for the selected target
+    st.dataframe(selected_target_df)
 
-    # Display the selected DataFrame in the app
-    st.dataframe(selected_data_df)
-
-    # Create a histogram of the selected column for the selected data type
-    fig = px.histogram(selected_data_df, x=select_box)
+    # Create a histogram of the selected column for the selected target
+    fig = px.histogram(selected_target_df, x='model', color='class_label')
     st.plotly_chart(fig)
 
 if __name__ == "__main__":
