0576d0adc6e15aa161d9e34aa8c9a7a61ca8c530
alleballer <albert.banke@gmail.com>
2023-05-30 13:19:33 +0200

    okok

--- app.py
+++ app.py
@@ -61,6 +61,12 @@ def main():
     avg_f1_by_data_fig = px.bar(avg_f1_by_data, x='data', y='macro_f1', title='Average Macro F1 Score per Data Type')
     st.plotly_chart(avg_f1_by_data_fig)
     
+    # Group by class label and calculate average macro_f1 score
+    avg_f1_by_class_label = selected_df.groupby('class_label')['macro_f1'].mean().reset_index()
+    avg_f1_by_class_label_fig = px.bar(avg_f1_by_class_label, x='class_label', y='macro_f1', 
+                                       title='Average Macro F1 Score per Class Label')
+    st.plotly_chart(avg_f1_by_class_label_fig)
+    
     # Group by model and CV method, then calculate average macro_f1 score
     avg_f1_by_model_cv = selected_df.groupby(['model', 'cv_method'])['macro_f1'].mean().reset_index()
     avg_f1_by_model_cv_fig = px.bar(avg_f1_by_model_cv, x='model', y='macro_f1', color='cv_method', 
